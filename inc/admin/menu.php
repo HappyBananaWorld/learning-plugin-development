05f2628887759037991bdba21dc95daa7ade1bdb
Mr Banana <sajad.dev.personal@gmail.com>
2025-07-13 19:19:03 +0330

    create menu

--- inc/admin/menu.php
+++ inc/admin/menu.php
@@ -1,16 +1,35 @@
 <?php
 
-add_action('admin_menu','lr_register_menu');
+add_action('admin_menu', 'lr_register_menu');
 
 function lr_register_menu(): void
 {
-    $title = "orders";
-    $page_title = "order";
+    $title = "learning plugin dashboard";
+    $page_title = "Dashboard";
     $role = "manage_options";
     $slug = "lr_orders";
-    $callback = 'lr_orders';
+    $callback = 'lr_dashboard_page';
     $icon_url = "";
     $position = 5;
 
     add_menu_page($page_title, $title, $role, $slug, $callback, $icon_url, $position);
+
+    // sub menu
+    $parent_slug = $slug;
+    $page_title = "add-option";
+    $menu_title = "add option";
+    $role = 'manage_options';
+    $menu_slug = 'add-option';
+    $callback = 'lr_add_option_page';
+    add_submenu_page($parent_slug, $page_title, $menu_title, $role, $menu_slug, $callback);
+}
+
+function lr_dashboard_page()
+{
+    echo "hello";
+}
+
+function lr_add_option_page()
+{
+    echo "hello";
 }
